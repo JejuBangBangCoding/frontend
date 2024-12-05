062513da91739d8ad2f21486d10833b22e497bc9
minsu kim <kimminsu31415@gmail.com>
2024-12-06 01:17:42 +0900

    fix: 코드 정리

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,35 +1,26 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import grayDot from "../assets/images/grayDot.svg";
 import whiteDot from "../assets/images/whiteDot.svg";
 import textLogo from "../assets/images/textLogo.svg";
 import mainLogo from "../assets/images/mainLogo.png";
 import rightArrow2 from "../assets/images/rightArrow2.svg";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function LandingPage() {
-  // const navigate = useNavigate();
-
   return (
-    // <div>
-    //   <h1 className="text-4xl font-bold text-red-600">LandingPage</h1>
-    //   <button
-    //     onClick={() => navigate("/loginpage")}
-    //     className="bg-blue-500 px-4 py-2 text-white"
-    //   >
-    //     Go to Login Page
-    //   </button>
-    // </div>
     <>
-      <div className="flex pt-12 gap-2 self-center mb-[5.4rem]">
+      <div className="mb-[5.4rem] flex gap-2 self-center pt-12">
         <img src={grayDot} alt="Gray Dot" className="w-2" />
         <img src={whiteDot} alt="White Dot" className="w-2" />
         <img src={whiteDot} alt="White Dot" className="w-2" />
       </div>
-      <img src={textLogo} alt="Text Logo" className="self-center w-[18.7rem]" />
+      <img src={textLogo} alt="Text Logo" className="w-[18.7rem] self-center" />
       <div className="relative mt-auto">
         <img src={mainLogo} alt="Main Logo" className="w-full" />
-        <Link to="/LandingPage2" className="absolute bottom-[25.2rem] right-[0.7rem] w-[1.2rem]">
+        <Link
+          to="/LandingPage2"
+          className="absolute bottom-[25.2rem] right-[0.7rem] w-[1.2rem]"
+        >
           <img src={rightArrow2} alt="Right Arrow2" className="" />
         </Link>
       </div>
--- src/pages/LandingPage2.js
+++ src/pages/LandingPage2.js
@@ -1,39 +1,57 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import whiteDot from "../assets/images/whiteDot.svg";
 import grayDot from "../assets/images/grayDot.svg";
 import smallLogo from "../assets/images/smallLogo.svg";
 import leftArrow1 from "../assets/images/leftArrow1.svg";
 import rightArrow1 from "../assets/images/rightArrow1.svg";
 import scene from "../assets/images/scene.png";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const LandingPage2 = () => {
-
   return (
     <>
-      <div className="flex pt-12 gap-2 self-center mb-[5.4rem]">
+      <div className="mb-[5.4rem] flex gap-2 self-center pt-12">
         <img src={whiteDot} alt="White Dot" className="w-2" />
         <img src={grayDot} alt="Gray Dot" className="w-2" />
         <img src={whiteDot} alt="White Dot" className="w-2" />
       </div>
-      <img src={smallLogo} alt="Small Logo" className="self-center w-[5.5rem] mb-7" />
+      <img
+        src={smallLogo}
+        alt="Small Logo"
+        className="mb-7 w-[5.5rem] self-center"
+      />
       <div className="text-center">
-        <p className="text-[2.5rem] font-black mb-3">놀앙뭐하젠:</p>
-        <p className="self-center text-[1rem] mb-[-0.3rem]">표준어로 <u><strong>‘놀아서 뭐 할래?’</strong></u>라는</p>
-        <p className="">의미가 담긴 <strong>제주어</strong></p>
+        <p className="mb-3 text-[2.5rem] font-black">놀앙뭐하젠:</p>
+        <p className="mb-[-0.3rem] self-center text-[1rem]">
+          표준어로{" "}
+          <u>
+            <strong>‘놀아서 뭐 할래?’</strong>
+          </u>
+          라는
+        </p>
+        <p className="">
+          의미가 담긴 <strong>제주어</strong>
+        </p>
       </div>
-      <div class="flex justify-between mx-[0.65rem] mb-12 mt-auto">
+      <div class="mx-[0.65rem] mb-12 mt-auto flex justify-between">
         <Link to="/LandingPage" className="">
-            <img src={leftArrow1} alt="Left Arrow1" className="bottom-[25.2rem] left-[0.7rem] w-[1.2rem]" />
+          <img
+            src={leftArrow1}
+            alt="Left Arrow1"
+            className="bottom-[25.2rem] left-[0.7rem] w-[1.2rem]"
+          />
         </Link>
         <Link to="/LandingPage3" className="">
-            <img src={rightArrow1} alt="Right Arrow1" className="bottom-[25.2rem] right-[0.7rem] w-[1.2rem]" />
+          <img
+            src={rightArrow1}
+            alt="Right Arrow1"
+            className="bottom-[25.2rem] right-[0.7rem] w-[1.2rem]"
+          />
         </Link>
       </div>
       <img src={scene} alt="Scene" className="w-full" />
     </>
   );
-}
+};
 
 export default LandingPage2;
