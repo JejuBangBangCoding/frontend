2a852768b490acf2a4fcc9290796264195519f3d
Green-JEONG <th2gr22n@gmail.com>
2024-12-14 15:29:40 +0900

    FarmListPage 프론트.. 80% 완료?
    (해야할 것)
    1. 버튼 위치 조정
    2. 상세 보기 버튼 고정
    3. 카드 슬라이더 무한 루프 적용

--- src/pages/FarmListPage.js
+++ src/pages/FarmListPage.js
@@ -3,6 +3,10 @@ import { useNavigate, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Card from "../components/Card";
 import bot from "../assets/images/bot.svg";
+import star from "../assets/images/star.svg";
+import prev from "../assets/images/prev.svg";
+import next from "../assets/images/next.svg";
+import location from "../assets/images/location.svg";
 
 function FarmListPage() {
   const navigate = useNavigate();
@@ -30,16 +34,16 @@ function FarmListPage() {
     <>
       <Header showProfile={true} showBackButton={true} />
       {/* 챗봇 */}
-      <div className="flex mt-16 gap-2 px-3">
+      <div className="flex mt-16 gap-2">
         <img src={bot} alt="Chat Bot" className="self-start" />
         <p className="bg-white px-3 py-2 rounded-b-3xl rounded-tr-3xl">애월에서 바다를 바라보며 일할 수 있는 로맨틱한 일자리 몇 가지를 준비했어요. 숙박도 제공되니 더 편하게 머무르실 수 있습니다!</p>
       </div>
 
       {/* 카드 슬라이드 */}
       <div className="mt-5 flex justify-center">
-        <div className="relative w-80 overflow-hidden">
+        <div className="relative w-80">
           <div
-            className="flex transition-transform duration-300"
+            className="flex transition-transform duration-300 gap-5"
             style={{
               transform: `translateX(-${currentIndex * 320}px)`,
               width: `${recommendations.length * 320}px`,
@@ -55,31 +59,29 @@ function FarmListPage() {
               </div>
             ))}
           </div>
+
+          {/* 이전 및 다음 버튼 */}
+          <button
+            onClick={handlePrev}
+            className="absolute top-1/2 left-0 transform -translate-y-1/2 shadow"
+          >
+            <img src={prev} alt="Previous" />
+          </button>
+          <button
+            onClick={handleNext}
+            className="absolute top-1/2 right-0 transform -translate-y-1/2 "
+          >
+            <img src={next} alt="Next" />
+          </button>
         </div>
       </div>
 
       {/* 페이지 번호 */}
-      <div className="mt-1 text-center">
+      {/* <div className="mt-1 text-center">
         <p className="text-gray-500">
           {currentIndex + 1} / {recommendations.length}
         </p>
-      </div>
-
-      {/* 이전 다음 버튼 */}
-      <div className="mt-4 flex justify-center gap-4">
-        <button
-          onClick={handlePrev}
-          className="rounded-full bg-gray-700 px-4 py-2 text-white"
-        >
-          &lt; Prev
-        </button>
-        <button
-          onClick={handleNext}
-          className="rounded-full bg-gray-700 px-4 py-2 text-white"
-        >
-          Next &gt;
-        </button>
-      </div>
+      </div> */}
     </>
   );
 }
